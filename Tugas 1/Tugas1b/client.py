c4297f9411b0b34e07a9d9b60e8ab9345e9337e1
IsnainiNurul <36990780+IsnainiNurul@users.noreply.github.com>
2020-02-18 08:18:42 +0700

    Update client.py

--- Tugas 1/Tugas1b/client.py
+++ Tugas 1/Tugas1b/client.py
@@ -10,24 +10,7 @@ server_address = ('localhost', 31001)
 print(f"connecting to {server_address}")
 sock.connect(server_address)
 
-#try:
-    # Send data
-#    message = 'makaneak'
-#    print(f"sending {message}")
-#    sock.sendall(message.encode())
-    # Look for the response
-#    amount_received = 0
-#    amount_expected = len(message)
-#    while amount_received < amount_expected:
-#        data = sock.recv(64)
-#        amount_received += len(data)
-#        print(f"{data}")
-#finally:
-#    print("closing")
-#    sock.close()
-#data = sock.recv(1024)
-#print('data=%s', (data))
-with open('received_file.txt', 'wb') as f:
+with open('file_terima.txt', 'wb') as f:
     print ('file opened')
     while True:
         print('receiving data...')
